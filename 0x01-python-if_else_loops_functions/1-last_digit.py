25c338327a5a2925296a053daed72127d5a5bdf1
CutiePizza <ines.chokri97@gmail.com>
2019-12-10 19:29:12 +0100

    fixed pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,12 @@ if number < 0:
     n = -10
 else:
     n = 10
-
+msg1 = "Last digit of {} is {} and is greater than 5"
+msg2 = "Last digit of {} is {} and is 0"
+msg3 = "Last digit of {} is {} and is less than 6 and not 0"
 if number % n > 5:
-    print("Last digit of {} is {} and is greater than 5"
-            .format(number, number % n))
+    print(msg1.format(number, number % n))
 elif number % n == 0:
-    print("Last digit of {} is {} and is 0"
-            .format(number, number % n))
+    print(msg2.format(number, number % n))
 elif number < 6:
-    print("Last digit of {} is {} and is less than 6 and not 0"
-            .format(number, number % n))
+    print(msg3.format(number, number % n))
