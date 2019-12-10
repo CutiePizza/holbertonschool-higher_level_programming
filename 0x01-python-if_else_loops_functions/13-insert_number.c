2927a929a81fac6ed6ecd4f7b3ac48b221f71733
CutiePizza <ines.chokri97@gmail.com>
2019-12-10 23:21:49 +0100

    glad

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 
 /**
+ * create_node - create a node
+ * @node: node
+ * @num: num
+ */
+
+void create_node(listint_t *node, int num)
+{
+	node = malloc(sizeof(listint_t));
+	node->n = num;
+	node->next = NULL;
+}
+
+/**
  * insert_node - insert a node in a list
  * @head: head of list
  * @number: number to add
@@ -12,13 +25,19 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *p;
+	listint_t *new = NULL, *p = NULL;
 
 	if (*head == NULL)
 	{
-		*head = malloc(sizeof(listint_t));
-		(*head)->n = number;
-		(*head)->next = NULL;
+		create_node(*head, number);
+		return (*head);
+	}
+	if (number < (*head)->n)
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = number;
+		new->next = *head;
+		(*head) = new;
 		return (*head);
 	}
 	p = *head;
