14b0264675ab8327b0be411a10b9a1395d041894
CutiePizza <ines.chokri97@gmail.com>
2019-12-10 20:08:03 +0100

    task 5 fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        print(i, end="")
         if j == 9 and i == 9:
-            print(j)
+            print("{:d}{:d}".format(i, j))
         else:
-            print(j, end=", ")
+            print("{:d}{:d}, ".format(i, j), end="")
