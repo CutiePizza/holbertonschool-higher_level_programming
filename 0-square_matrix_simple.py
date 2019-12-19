7094a03044d7c90e675777983ef93b9b1cbf59cc
CutiePizza <ines.chokri97@gmail.com>
2019-12-19 18:04:25 +0100

    removing file

--- 0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new = []
-    for j in range(len(matrix)):
-        mini = []
-        for k in range(len(matrix[j])):
-            mini.append(matrix[j][k] ** 2)
-        new.append(mini)
-    return (new)
