ed1761272a3d42ac21b9759110eec9cab95fdb03
CutiePizza <ines.chokri97@gmail.com>
2020-01-08 16:59:06 +0100

    Added exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format())
+        print("{:d}".format(value))
     except IndexError:
+        pass
+    except ValueError:
         return False
     return True
