01c97f88329c574beb1cf2ef388b381fe4a42df3
CutiePizza <ines.chokri97@gmail.com>
2020-01-08 16:40:04 +0100

    added an execption

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print("")
         return i + 1
-    except TypeError:
-            print("")
-            return i
+    except (TypeError, IndexError):
+        pass
+    print("")
+    return i
