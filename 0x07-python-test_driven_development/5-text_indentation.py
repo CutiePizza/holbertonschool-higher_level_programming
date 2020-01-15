2141f039d21eeb78a5f81ef1c0ec6b9d47a80b70
CutiePizza <ines.chokri97@gmail.com>
2020-01-15 23:42:15 +0100

    solved task

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,15 +10,19 @@ def text_indentation(text):
     """
     Function that prints My name and last name
     """
-
     if type(text) is not str or len(text) == 0:
         raise TypeError("text must be a string")
-    print(text[0], end="")
-    for i in range(len(text)):
+    i = 0
+    while i != len(text):
         if text[i] == "?" or text[i] == ":" or text[i] == ".":
-            print("\n")
+            print(text[i])
+            print("")
         else:
-            try:
+            if (text[i - 1] == "?" or text[i - 1] == ":" or text[i - 1] == ".")
+            and
+            (text[i] == " "):
                 print(text[i + 1], end="")
-            except IndexError:
-                pass
+                i += 1
+            else:
+                print(text[i], end="")
+        i = i + 1
