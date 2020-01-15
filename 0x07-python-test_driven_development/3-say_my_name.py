acc04fc8c0bf18f1727f8fdd297b29923e3a184e
CutiePizza <ines.chokri97@gmail.com>
2020-01-15 01:36:14 +0100

    task 3 done

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module with a function say_my_name
+Import this module in your file to use the function say_my_name
+Example: See docstring
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints My name and last name
+    """
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        if len(last_name) == 0:
+            print("My name is {} ".format(first_name))
+        else:
+            print("My name is {} {}".format(first_name, last_name))
+    else:
+        if not isinstance(first_name, str) or first_name is None:
+            raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
