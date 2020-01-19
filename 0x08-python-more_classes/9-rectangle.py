4ec05caa1f18576ff4d4b385b620aa0ef387f890
CutiePizza <ines.chokri97@gmail.com>
2020-01-19 23:44:26 +0100

    changed isinstance by type

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -13,6 +13,16 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    def __init__(self, width=0, height=0):
+        """Rectangle's constructor
+        Args:
+            width (int): default value 0
+            height (int): default value 0
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         """Retrieve rectangle's width
@@ -27,11 +37,12 @@ class Rectangle:
         Args:
         value (int): Rectangle's width
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -47,25 +58,12 @@ class Rectangle:
         Args:
         value (int): Rectangle's height
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def __init__(self, width=0, height=0):
-        """Rectangle's constructor
-        Args:
-            width (int): default value 0
-            height (int): default value 0
-        """
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
-        self.__width = width
-        Rectangle.number_of_instances += 1
+        else:
+            self.__height = value
 
     def area(self):
         """Calculate the rectangle's area
