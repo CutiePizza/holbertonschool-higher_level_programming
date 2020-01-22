6ca3ae5338a3b9213f0db70dd627404a818d8e2a
CutiePizza <ines.chokri97@gmail.com>
2020-01-22 19:04:23 +0100

    task 14 done/ Pascal"s Triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Print a pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return ([])
+    mat = [[1]]
+    val = 1
+    for i in range(1, n):
+        mini = []
+        for j in range(i + 1):
+            if j is 0:
+                val = 1
+            else:
+                val = val * (i - j + 1)/j
+            mini.append(int(val))
+        mat.append(mini)
+
+    return (mat)
