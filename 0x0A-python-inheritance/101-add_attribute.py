a6bb07b848e89214f161481711df550da8e38424
CutiePizza <ines.chokri97@gmail.com>
2020-01-22 10:20:32 +0100

    added /usr at the top

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,8 @@ Functions that adds attribute to an object
 
 
 def add_attribute(obj, key, value):
-    if not hasattr(obj, key):
-        setattr(obj, key, value)
+    my_tuple = (float, int, str, bool)
+    if isinstance(obj, my_tuple):
+        obj.key = value
     else:
         raise TypeError("can't add new attribute")
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Function is_kind_of_class
 """
