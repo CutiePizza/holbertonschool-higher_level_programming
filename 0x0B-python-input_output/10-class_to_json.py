702643cd2397c9a6523537cae0308dd88b836811
CutiePizza <ines.chokri97@gmail.com>
2020-01-22 19:01:56 +0100

    task 10 done/ Function that returns a dict of a class

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for class t json
+"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    of an object
+    """
+    a = obj.__dict__
+    return (dict(a))
