238ff30cdc45ce73d1d4d14745c0aacb1445daa1
CutiePizza <ines.chokri97@gmail.com>
2020-01-22 21:50:56 +0100

    task 13 done

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """Class Student
+    Attributes:
+        first_name: first name
+        last_name: last name
+        age: age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initilize attribute
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dict
+        """
+        a_dict = {}
+        if attrs is not None:
+            for i in range(len(attrs)):
+                if attrs[i] in self.__dict__:
+                    a_dict[attrs[i]] = self.__dict__[attrs[i]]
+            return a_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json[first_name]
+        self.last_name = json[last_name]
+        self.age = json[age]
