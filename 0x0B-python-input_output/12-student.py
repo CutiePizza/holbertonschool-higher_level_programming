456398c3928ebf63a5c0d20553c43dc24d3fe081
CutiePizza <ines.chokri97@gmail.com>
2020-01-22 19:03:36 +0100

    Not done yet/ Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """Class Student
+    Attributes:
+        first_name: first name
+        last_name: last name
+        age: age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initilize attribute
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dict
+        """
+        if attrs is not None:
+            list_i = []
+        return(dict(self.__dict__))
