3456a1a2168c3e02f0f92278d3f344f413142b25
CutiePizza <ines.chokri97@gmail.com>
2020-01-28 19:30:04 +0100

    task 15 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        To json dictionary
+        """
+        import json
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return (json.dumps(list_dictionaries))
