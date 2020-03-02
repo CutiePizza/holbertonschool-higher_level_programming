4556dffc6b613a951a0214a19e488123ff0824bc
CutiePizza <ines.chokri97@gmail.com>
2020-03-02 21:02:07 +0100

    fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- script that displays the average temperatures
-SELECT city, AVG(value) as avg_tmp FROM temperatures GROUP BY city;
+SELECT city, AVG(value) as avg_tmp FROM temperatures GROUP BY city ORDER BY city DESC;
