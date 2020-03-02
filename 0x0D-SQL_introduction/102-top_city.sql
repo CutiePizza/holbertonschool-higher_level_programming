c1eae47f1e86b311e85dd7343d4daeeff5190d2a
CutiePizza <ines.chokri97@gmail.com>
2020-03-02 21:59:42 +0100

    missing letter add

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Top three
-SELECT city, AVG(value) as avg_tmp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
