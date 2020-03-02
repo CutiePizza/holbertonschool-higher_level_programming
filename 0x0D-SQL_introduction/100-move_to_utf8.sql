c9de483fa9039bb79f4403db5a8d5925670c01ee
CutiePizza <ines.chokri97@gmail.com>
2020-03-02 20:35:27 +0100

    fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
 -- Converts a database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+UPDATE first_table
+SET name=CONVERT USING utf8;
