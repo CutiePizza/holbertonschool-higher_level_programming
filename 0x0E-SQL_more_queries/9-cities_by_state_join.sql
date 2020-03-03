8d2374e6209c198b47e14daf6370d6a229ceaba3
CutiePizza <ines.chokri97@gmail.com>
2020-03-03 22:38:30 +0100

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Join
 -- Cities
-SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.id=states.state_id
+SELECT cities.id AS id, cities.name AS name, states.name AS name FROM cities
+JOIN states ON cities.id=states.state_id
 ORDER BY cities.id ASC;
