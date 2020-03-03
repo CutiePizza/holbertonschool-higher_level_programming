9dc6a547e7486e230378c3a3b4a8f3f5e1da7b20
CutiePizza <ines.chokri97@gmail.com>
2020-03-03 20:32:21 +0100

    task 6 done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- creates a tables
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id int UNIQUE NOT NULL AUTO_INCREMENT, name varchar(256), PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS states (id int UNIQUE NOT NULL AUTO_INCREMENT, name varchar(256) NOT NULL, PRIMARY KEY (id));
