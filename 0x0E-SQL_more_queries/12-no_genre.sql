b4c36f8a78a3c2ad53df17d53a0a0b0b35c96e93
CutiePizza <ines.chokri97@gmail.com>
2020-03-04 00:05:10 +0100

    task done

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,3 @@
 -- list all shows
 -- in hbtn_0d_tvshows
-SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC WHERE tv_show_genres.show_id IS NULL OR tv_shows.id IS NULL;
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id FROM tv_show_genres LEFT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC WHERE tv_show_genres.show_id IS NULL OR tv_shows.id IS NULL;
