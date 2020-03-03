d17812d7d267655998685ca7e23cd577ff74e1ab
CutiePizza <ines.chokri97@gmail.com>
2020-03-03 21:05:56 +0100

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- creates a database
 -- creates a tables
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-	id int UNIQUE NOT NULL AUTO_INCREMENT,
-	state_id NOT NULL,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id int NOT NULL AUTO_INCREMENT,
+	state_id int NOT NULL,
 	name varchar(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY(state_id)
