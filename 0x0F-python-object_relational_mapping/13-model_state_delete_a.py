97c3e530c931281c921dc4f197a968925669e253
CutiePizza <ines.chokri97@gmail.com>
2020-03-28 21:48:55 +0100

    fix

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -22,3 +22,5 @@ if __name__ == "__main__":
     for row in session.query(State).filter(State.name.like('%a%')):
         session.delete(row)
         session.commit()
+    cursor.close()
+    db.close()
