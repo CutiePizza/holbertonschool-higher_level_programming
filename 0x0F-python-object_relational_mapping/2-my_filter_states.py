42159ec6ce8fea90b3704f21fa6db4ad0af136f6
CutiePizza <ines.chokri97@gmail.com>
2020-03-28 13:52:31 +0100

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,14 +2,11 @@
 """
 Order by user input
 """
-
-
 if __name__ == "__main__":
     import MySQLdb
     import sys
     db = MySQLdb.connect(
             host="localhost",
-            port=3306,
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
