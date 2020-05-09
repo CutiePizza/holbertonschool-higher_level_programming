3df962b19913bd973793d720389293a80ec500b1
CutiePizza <ines.chokri97@gmail.com>
2020-05-09 18:27:43 +0100

    convert arg

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log('Not a number');
+} else if (process.argv.length === 3) {
+  if (isNaN(process.argv[2])) {
+    console.log('Not a number');
+  } else {
+    console.log(parseInt(process.argv[2]));
+  }
+}
