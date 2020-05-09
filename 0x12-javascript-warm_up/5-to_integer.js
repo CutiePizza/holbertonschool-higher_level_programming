134920d94fada038d916b2b66de43d883ea23854
CutiePizza <ines.chokri97@gmail.com>
2020-05-09 18:30:13 +0100

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('Not a number');
-} else if (process.argv.length === 3) {
+} else if ('My number: ' + process.argv.length === 3) {
   if (isNaN(process.argv[2])) {
     console.log('Not a number');
   } else {
