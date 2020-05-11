f5058ae628e9b2ebbcaf75ee136c6460bb82cfe8
CutiePizza <ines.chokri97@gmail.com>
2020-05-11 17:44:37 +0100

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      for (j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+};
