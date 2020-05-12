017bb309fa10505c6d7632b9b5a176d43237d386
CutiePizza <ines.chokri97@gmail.com>
2020-05-12 21:14:03 +0100

    fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const url = process.argv[2];
 const filename = process.argv[3];
 request(url, { json: true }, (err, res, body) => {
-  if (err) { console.log(err); }
-  const string = body;
-  fs.writeFile(filename, string, 'utf8', (error) => {
-    if (error) console.log(error);
-  });
+  if (err) { console.log(err); } else {
+    const string = body;
+    fs.writeFile(filename, string, 'utf8', (error) => {
+      if (error) console.log(error);
+    });
+  }
 });
