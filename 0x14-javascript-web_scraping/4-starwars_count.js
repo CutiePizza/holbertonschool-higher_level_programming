76810aed0443974677c0ddafbd18c40edecaf359
CutiePizza <ines.chokri97@gmail.com>
2020-05-13 21:55:14 +0100

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 request(url, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   let i, j;
